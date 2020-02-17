9310948e6177bb6e55df1639f8c65b783523f735
cheriko <marie.uglova@yandex.ru>
2020-02-17 15:04:48 +0300

    Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 var PIN_HEIGHT_TAIL = 84;
+var MOUSE_KEY = 0;
 var titles = [
   'Заголовок объявления 1',
   'Заголовок объявления 2',
@@ -16,20 +17,25 @@ var titles = [
 
 var mapPinsElement = document.querySelector('.map__pins');
 var mapPinsWidth = mapPinsElement.offsetWidth;
-var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var mapPinMain = document.querySelector('.map__pin--main');
-var mapFilter = document.querySelector('.map');
-var mainForm = document.querySelector('.ad-form');
+var mapPinTemplateElement = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPinMainElement = document.querySelector('.map__pin--main');
+var mapFilterElement = document.querySelector('.map');
+var mainFormElement = document.querySelector('.ad-form');
 var allInputsAndSelects = document.querySelectorAll('.ad-form fieldset');
 var inputAddress = document.querySelector('#address');
 var submit = document.querySelector('.ad-form__submit');
-var mapPinMainPositionLeft = Math.floor(mapPinMain.offsetLeft + mapPinMain.offsetWidth / 2);
-var mapPinMainPositionTop = Math.floor(mapPinMain.offsetTop - mapPinMain.offsetHeight / 2);
-var mapPinMainPositionTopTail = Math.floor(mapPinMain.offsetTop + PIN_HEIGHT_TAIL);
+var selectRoomNumber = document.querySelector('#room_number');
+var selectCapacity = document.querySelector('#capacity');
+var mapPinMainPositionLeft = Math.floor(mapPinMainElement.offsetLeft + mapPinMainElement.offsetWidth / 2);
+var mapPinMainPositionTop = Math.floor(mapPinMainElement.offsetTop - mapPinMainElement.offsetHeight / 2);
+var mapPinMainPositionTopTail = Math.floor(mapPinMainElement.offsetTop + PIN_HEIGHT_TAIL);
 
-for (var i = 0; i < allInputsAndSelects.length; i++) {
-  allInputsAndSelects[i].setAttribute('disabled', 'disabled');
-}
+var disableInputsAndSelects = function () {
+  for (var i = 0; i < allInputsAndSelects.length; i++) {
+    allInputsAndSelects[i].setAttribute('disabled', 'disabled');
+  }
+};
+disableInputsAndSelects();
 
 var randomizeInteger = function (min, max) {
   var rand = min + Math.random() * (max - min);
@@ -70,7 +76,7 @@ var advertisements = generateAdvertisements();
 
 /* клонируем mapPin и создаем свойства */
 var createMapPin = function (advertisement) {
-  var mapPinElement = mapPinTemplate.cloneNode(true);
+  var mapPinElement = mapPinTemplateElement.cloneNode(true);
   mapPinElement.querySelector('img').setAttribute('src', advertisement.author.avatar);
   mapPinElement.querySelector('img').setAttribute('alt', advertisement.offer.title);
   mapPinElement.style.left = advertisement.location.x - PIN_WIDTH / 2 + 'px';
@@ -91,10 +97,9 @@ var renderAdvertisements = function (array) {
 renderAdvertisements(advertisements);
 
 var activateInputsAndSelects = function () {
-  allInputsAndSelects.forEach(
-      function (el) {
-        el.removeAttribute('disabled');
-      }
+  allInputsAndSelects.forEach(function (el) {
+    el.removeAttribute('disabled');
+  }
   );
 };
 
@@ -107,34 +112,30 @@ var getMapPinMainCoors = function () {
 var activateForm = function () {
   activateInputsAndSelects();
   getMapPinMainCoors();
-  mapFilter.classList.remove('map--faded');
-  mainForm.classList.remove('ad-form--disabled');
+  mapFilterElement.classList.remove('map--faded');
+  mainFormElement.classList.remove('ad-form--disabled');
 };
 
-mapPinMain.addEventListener('mousedown', function (evt) {
-  if (evt.button === 0) {
+mapPinMainElement.addEventListener('mousedown', function (evt) {
+  if (evt.button === MOUSE_KEY) {
     activateForm();
   }
 });
 
-mapPinMain.addEventListener('keydown', function (evt) {
+mapPinMainElement.addEventListener('keydown', function (evt) {
   if (evt.code === 'Enter') {
     activateForm();
   }
 });
 
-var checkRoomsAndCapacity = function (sel) {
-  var selectRoomNumber = document.querySelector('#room_number');
-  var selectCapacity = document.querySelector('#capacity');
-  if (selectRoomNumber.value !== selectCapacity.value) {
+var validateRoomsAndCapacity = function (sel) {
+  if (selectRoomNumber.value >= selectCapacity.value) {
+    sel.setCustomValidity('');
+  } else {
     sel.setCustomValidity('Что-то тут не так');
   }
 };
 
 submit.addEventListener('click', function () {
-  var roomsAndCapacity = document.querySelectorAll('#room_number, #capacity');
-  for (var i = 0; i < roomsAndCapacity.length; i++) {
-    var select = roomsAndCapacity[i];
-    checkRoomsAndCapacity(select);
-  }
+  validateRoomsAndCapacity(selectRoomNumber);
 });
